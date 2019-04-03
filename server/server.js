f88fe783a29e6bc49c766593bbb52a415f157dac
andrescerv <44825347+andrescerv@users.noreply.github.com>
2019-04-02 20:19:16 -0600

    iteración 1.1 completa

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
 const express = require('express')
+const cookieParser = require('cookie-parser')
+
 const app = express()
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+mongoose.connect(process.env.DATABASE, {useNewUrlParser: true}, (err) => {
+  if(err) return err
+  console.log("Conectado a MongoDB")
+})
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cookieParser())
+
 const port = process.env.PORT || 3002
 
 app.listen(port, () => {
